fb415c70798b79eb34d8510090c76d0ec0a2fb52
Syafa Ramadhan <144409844+Syauqi7@users.noreply.github.com>
2023-10-19 13:17:24 +0700

    Add files via upload

---
+++ database/myapp_syafa.sql
@@ -0,0 +1,96 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 19 Okt 2023 pada 07.56
+-- Versi server: 10.4.24-MariaDB
+-- Versi PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `myapp_syafa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `hero`
+--
+
+CREATE TABLE `hero` (
+  `idHero` int(11) NOT NULL,
+  `gambar` varchar(255) NOT NULL,
+  `judul` varchar(255) NOT NULL,
+  `subjudul` text NOT NULL,
+  `status` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `hero`
+--
+
+INSERT INTO `hero` (`idHero`, `gambar`, `judul`, `subjudul`, `status`) VALUES
+(1, 'WhatsApp Image 2023-10-18 at 2.08.08 PM.jpeg', 'ARGRAAAA', 'ARGRAA MANDIIIII', 'aktif'),
+(2, 'WhatsApp Image 2023-10-18 at 2.09.17 PM.jpeg', 'AYYYYAMMMM', 'AYAM AYAM AYAM', 'aktif');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `idUser` varchar(20) NOT NULL,
+  `nmUser` varchar(50) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `level` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`idUser`, `nmUser`, `username`, `password`, `level`) VALUES
+('1', 'Supri', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `hero`
+--
+ALTER TABLE `hero`
+  ADD PRIMARY KEY (`idHero`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`idUser`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `hero`
+--
+ALTER TABLE `hero`
+  MODIFY `idHero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
